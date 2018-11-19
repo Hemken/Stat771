e62d3f2295c6a8833a40a85129dfd1eb8c873df4
D Hemken <dehemken@wisc.edu>
2018-11-19 14:48:48 -0600

    more logistic

---
+++ hw/reshape UCBAdmit.r
@@ -0,0 +1,59 @@
+Admit <- UCBAdmissions
+Admit[1,,]
+Admit[2,,]
+Admit[,1,]
+Admit[,2,]
+
+df <- matrix(NA, ncol=4, nrow=24)
+l <- 0
+for (i in 1:2) {
+  admit <- 2-i
+  for (j in 1:2) {
+    female <- j - 1
+    for (k in 1:6) {
+      dept <- k
+      l <- l + 1
+      df[l,] <- c(admit,female,dept,Admit[i,j,k])
+      
+    }
+  }
+}
+
+df <- as.data.frame(df)
+names(df) <- c("admit", "female", "dept", "count")
+
+df$dept <- factor(df$dept, labels=LETTERS[1:6])
+# df$deptA <- as.integer(df$dept=="A")
+# df$deptB <- as.integer(df$dept=="B")
+# df$deptC <- as.integer(df$dept=="C")
+# df$deptD <- as.integer(df$dept=="D")
+# df$deptE <- as.integer(df$dept=="E")
+# df$deptF <- as.integer(df$dept=="F")
+# df$Cons <- 1
+
+fit <- glm(admit~female+dept, weight=count, data=df, family="binomial")
+summary(fit)
+
+X <- model.matrix(~female+dept, data=df)
+X
+
+B <- rep(1,7)
+Y <- df$admit
+counts <- df$count
+admitll <- function(B,Y,X,counts){
+  sum(counts*(Y*(X%*%B)- log(1 + exp(X%*%B))))
+}
+
+admitll(B,Y,X,counts)
+
+admitll(coef(fit),Y,X,counts)
+admitll(coef(fit)+.01,Y,X,counts)
+admitll(coef(fit)-.01,Y,X,counts)
+
+admitgr <- function(B,Y,X,counts){
+  for (i in 1:5){
+    print((Y[i]-1/(1+exp(-X[i,]%*%B))))
+    
+  }
+}
+admitgr(B,Y,X,counts)
