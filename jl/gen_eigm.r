7cd6f3942344c47a41de5ec3a9c11c665e2699b5
D Hemken <dehemken@wisc.edu>
2018-11-23 14:43:46 -0600

    Newton Method for Non-linear

--- jl/gen_eigm.r
+++ jl/gen_eigm.r
@@ -3,26 +3,24 @@ gen_eigm <- function(d) {
   # returns a matrix with those eigenvalues
   sigma = diag(d) # diagonal matrix of eigenvalues
   nm = dim(sigma)
-  V = matrix(rnorm(prod(nm)), ncol=nm[2])   # random matrix
+  V = matrix(runif(prod(nm), -5, 5), ncol=nm[2])   # random matrix
   A = solve(V)%*%sigma%*%V        # random A with given eigenvalues
   return(A)
 }
 
-
-gen_eigm(c(1,1))
-evals <- c(2,0.3)
+evals <- c(2,-2)
 A <- gen_eigm(evals)
 SVDA <- svd(A)
-# t(SVDA$u) %*% diag(SVDA$d) %*% SVDA$v
+# SVDA$u %*% diag(SVDA$d) %*% t(SVDA$v)
 EIGA <- eigen(A)
 
 SVDA$d;EIGA$values
 
-norm(as.matrix(SVDA$d));norm(as.matrix(EIGA$values))
+prod(SVDA$d);prod(EIGA$values)
 
 n <- 50
-evals <- c(2,0.3)
-SVDA <- matrix(NA, ncol=2, nrow=n)
+evals <- c(2,-0.3)
+SVDA <- matrix(NA, ncol=length(evals), nrow=n)
 for (i in 1:n) {
   A <- gen_eigm(evals)
   SVDA[i,] <- svd(A)$d
@@ -30,3 +28,12 @@ for (i in 1:n) {
 plot(rbind(evals,SVDA))
 plot(SVDA[,1]*SVDA[,2])
      
+n <- 50
+evals <- c(2,0.5,-0.3)
+SVDA <- matrix(NA, ncol=length(evals), nrow=n)
+for (i in 1:n) {
+  A <- gen_eigm(evals)
+  SVDA[i,] <- svd(A)$d
+}
+#plot(rbind(evals,SVDA))
+plot(SVDA[,1]*SVDA[,2]*SVDA[,3])
